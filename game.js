92d001792a3cf4fbd385adbd1b7bac48eaef1efe
TriniRs <triniirs@gmail.com>
2024-07-03 12:30:21 -0300

    Seguimiento app.js

--- game.js
+++ game.js
@@ -5,4 +5,18 @@ document.getElementById("startGameButton").onclick = function() {
     const errorMessage = document.getElementById('error-message');
 
     const mailPattern = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
-}
+
+if (name.length >= 3 && mailPattern.test(mail) && time) {
+    localStorage.setItem('namePlayer', name);
+    window.location.href = `game.html?time=${time}`;
+} else {
+    if (name.length < 3) {
+        errorMessage.textContent = "Too short (3 letters minimum)";
+    } else if (!mailPattern.test(mail)) {
+        errorMessage.textContent = "Invalid email";
+    } else {
+        errorMessage.textContent = "Complete all the fields";
+    }
+    errorMessage.style.display = "block";
+} 
+};
